21ad02a42829f29ddc386a4834f83926140a250d
Daniel Zawadzki <hello@danielzawadzki.com>
2019-03-11 22:14:35 +0100

    Added App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tooltip from "./components/Tooltip/Tooltip";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+export interface IAppProps {}
+
+/**
+ * App component.
+ *
+ * @author Daniel Zawadzki <hello@danielzawadzki.com>
+ * @version 1.0.0
+ */
+const App: React.FunctionComponent<IAppProps> = ({}) => (
+    <div className="App">
+        <Header/>
+
+        {/* Tooltip 1 */}
+        <p>
+            <Tooltip message="Howdy-ho, I'm an example tooltip 🤗!" position="top" key="tooltip-1">
+                Tooltip above
+            </Tooltip>
+        </p>
+
+        {/* Tooltip 2 */}
+        <p>
+            <Tooltip message="Howdy-ho, I'm an example tooltip 🤗!" position="bottom" key="tooltip-2">
+                Tooltip under
+            </Tooltip>
+        </p>
+
+        {/* Tooltip 3 */}
+        <p>
+            <Tooltip message="Howdy-ho, I'm an example tooltip 🤗!" position="right" key="tooltip-3">
+                Tooltip on right
+            </Tooltip>
+        </p>
+
+        {/* Tooltip 4 */}
+        <p>
+            <Tooltip message="Howdy-ho, I'm an example tooltip 🤗!" position="left" key="tooltip-4">
+                Tooltip on left
+            </Tooltip>
+        </p>
+
+        <Footer/>
+    </div>
+);
+
 
 export default App;
