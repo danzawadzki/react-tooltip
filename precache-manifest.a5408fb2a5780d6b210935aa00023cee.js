25338b54fb7f4dc8dbd5a2987fc70b76973a158b
Daniel Zawadzki <hello@danielzawadzki.com>
2019-03-11 22:29:36 +0100

    Updates

---
+++ precache-manifest.a5408fb2a5780d6b210935aa00023cee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "074e3967f00be3c513e2",
+    "url": "/react-tooltip/static/js/runtime~main.0bdf53a6.js"
+  },
+  {
+    "revision": "5d74ab0257a3a89d076f",
+    "url": "/react-tooltip/static/js/main.eb319bba.chunk.js"
+  },
+  {
+    "revision": "61eeb233e98d52102207",
+    "url": "/react-tooltip/static/js/2.680cff78.chunk.js"
+  },
+  {
+    "revision": "5d74ab0257a3a89d076f",
+    "url": "/react-tooltip/static/css/main.4aea9912.chunk.css"
+  },
+  {
+    "revision": "fb45256bb6bd002b11b7358547b3e93d",
+    "url": "/react-tooltip/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-tooltip/precache-manifest.a5408fb2a5780d6b210935aa00023cee.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-tooltip/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
